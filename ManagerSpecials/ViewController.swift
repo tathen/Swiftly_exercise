c3c91904b2a314492f275412d28ccf87ca21a3c8
Sean Howell <tathen@yahoo.com>
2020-11-30 17:13:01 -0600

    Initial Commit

---
+++ ManagerSpecials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManagerSpecials
+//
+//  Created by Sean Howell on 11/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
