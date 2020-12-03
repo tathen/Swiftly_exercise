1623797136e14b410a94e844a1a4548b30615b40
Sean Howell <tathen@yahoo.com>
2020-12-02 20:46:28 -0600

    Use NSCollectionLayoutGroup for groupedItems

--- ManagerSpecials/ManagerSpecialCollectionViewController.swift
+++ ManagerSpecials/ManagerSpecialCollectionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 private let endPoint = "https://raw.githubusercontent.com/Swiftly-Systems/code-exercise-ios/master/backup"
+private let padding: CGFloat = 8
 
 class ManagerSpecialCollectionViewController: UICollectionViewController {
 
@@ -21,9 +22,6 @@ class ManagerSpecialCollectionViewController: UICollectionViewController {
         // Populate Data (TODO: move networking outside of this view controller)
         do {
             let data = try Data(contentsOf: URL(string: endPoint)!)
-            
-//            let path = Bundle.main.path(forResource: "SampleData", ofType: nil)
-//            let data = try Data(contentsOf: URL(fileURLWithPath: path!))
             
             let decoder = JSONDecoder()
             let endPointResponse = try decoder.decode(EndPointResponse.self, from: data)
@@ -43,24 +41,20 @@ class ManagerSpecialCollectionViewController: UICollectionViewController {
             let size = NSCollectionLayoutSize(widthDimension: .absolute(fractionalWidth),
                                               heightDimension: .absolute(fractionalHeight))
             let collectionItem = NSCollectionLayoutItem(layoutSize: size)
-            let padding: CGFloat = 8
             collectionItem.contentInsets = NSDirectionalEdgeInsets(top: padding, leading: padding, bottom: padding, trailing: padding)
             layoutItems.append(collectionItem)
         }
-        var groupedItems: [NSCollectionLayoutItem] =  buildGroups(from: layoutItems)
-       
+        var groupedItems: [NSCollectionLayoutGroup] =  buildGroups(from: layoutItems)
        
         
         // center groups
         groupedItems.forEach { (group) in
-//            let symetricalInset = (collectionView.safeAreaLayoutGuide.layoutFrame.width - group.layoutSize.widthDimension.dimension) / 2
-//            group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: symetricalInset, bottom: 0, trailing: symetricalInset)
-            
+//            group.edgeSpacing = NSCollectionLayoutEdgeSpacing(leading: .flexible(padding), top: nil, trailing: .flexible(padding), bottom: nil)
         }
         
         // apply to section
         guard groupedItems.isEmpty == false else { return }
-        let allItemsSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.66),
+        let allItemsSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
                                                heightDimension: .estimated(100))
         let allItemsGroup = NSCollectionLayoutGroup.vertical(layoutSize: allItemsSize, subitems: groupedItems)
         let section = NSCollectionLayoutSection(group: allItemsGroup)
@@ -72,18 +66,20 @@ class ManagerSpecialCollectionViewController: UICollectionViewController {
        
     }
     //    TODO: move this func
-    func buildGroups(from items: [NSCollectionLayoutItem]) -> [NSCollectionLayoutItem] {
+    func buildGroups(from items: [NSCollectionLayoutItem]) -> [NSCollectionLayoutGroup] {
         var iterator = items.makeIterator()
         guard let item = iterator.next() else { return [] }
         
-        var result = [NSCollectionLayoutItem]()
+        var result = [NSCollectionLayoutGroup]()
         // populate results recursively
         // TODO: rename this func to reflect it populates 'result'
         func buildLayoutItem(for item: NSCollectionLayoutItem) {
             // base case
             guard let nextItem = iterator.next() else {
                 // package up item to result
-                result.append(item)
+                let size = item.layoutSize
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+                result.append(group)
                 return
             }
             
@@ -91,13 +87,13 @@ class ManagerSpecialCollectionViewController: UICollectionViewController {
             let combinedItemWidth = nextItem.layoutSize.widthDimension.dimension + item.layoutSize.widthDimension.dimension
             if combinedItemWidth <= collectionView.safeAreaLayoutGuide.layoutFrame.width {
                 // combine item + nextItem into subgroup and recurse
-                let subgroupSize = NSCollectionLayoutSize(widthDimension: .estimated(200), heightDimension: .estimated(200))
+                let subgroupSize = NSCollectionLayoutSize(widthDimension: .absolute(combinedItemWidth), heightDimension: .estimated(200))
                 let subgroup = NSCollectionLayoutGroup.horizontal(layoutSize: subgroupSize, subitems: [item, nextItem])
                 buildLayoutItem(for: subgroup)
             } else {
                 // reached line break
-                
-                result.append(item)
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: item.layoutSize, subitems: [item])
+                result.append(group)
                 buildLayoutItem(for: nextItem)
             }
         }
