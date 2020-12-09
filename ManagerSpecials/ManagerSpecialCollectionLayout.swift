852b576de964d1f321ca617270c937c08c44e2fc
Sean Howell <tathen@yahoo.com>
2020-12-09 16:08:30 -0600

    extract layoutItem(for:canvasUnitPointSize:)

--- ManagerSpecials/ManagerSpecialCollectionLayout.swift
+++ ManagerSpecials/ManagerSpecialCollectionLayout.swift
@@ -49,17 +49,34 @@ func groupedRows(from items: [NSCollectionLayoutItem], width: CGFloat) -> [NSCol
     return rowGroups
 }
 
+
+/// Create the layout item with
+/// - Parameters:
+///   - item: The DiscountItem
+///   - canvasUnitPointSize: The CanvasUnit in points
+/// - Returns: The correctly sized and indented CollectionLayoutItem
+func layoutItem(for item: DiscountItem, canvasUnitPointSize: CGFloat) -> NSCollectionLayoutItem {
+    let itemWidth = canvasUnitPointSize * CGFloat(item.width)
+    let itemHeight = canvasUnitPointSize * CGFloat(item.height)
+    let size = NSCollectionLayoutSize(widthDimension: .absolute(itemWidth),
+                                      heightDimension: .absolute(itemHeight))
+    let collectionItem = NSCollectionLayoutItem(layoutSize: size)
+    collectionItem.contentInsets = NSDirectionalEdgeInsets(top: padding, leading: padding, bottom: padding, trailing: padding)
+
+    return collectionItem
+
+}
+
+/// Creates the layout for use in ManagerSpecialCollectionViewController
+/// - Parameters:
+///   - discountItems: The items on sale
+///   - width: The width of the layout
+///   - partitionCount: The number of CanvaUnits
+/// - Returns: A new CollectionView Layout
 func collectionLayout(for discountItems: [DiscountItem], width: CGFloat, partitionCount: CanvasUnit) -> UICollectionViewLayout {
-    var layoutItems = [NSCollectionLayoutItem]()
-    let partitionSize = width / CGFloat(partitionCount)
-    for item in discountItems { //TODO: use .map()
-        let fractionalWidth = partitionSize * CGFloat(item.width)
-        let fractionalHeight = partitionSize * CGFloat(item.height)
-        let size = NSCollectionLayoutSize(widthDimension: .absolute(fractionalWidth),
-                                          heightDimension: .absolute(fractionalHeight))
-        let collectionItem = NSCollectionLayoutItem(layoutSize: size)
-        collectionItem.contentInsets = NSDirectionalEdgeInsets(top: padding, leading: padding, bottom: padding, trailing: padding)
-        layoutItems.append(collectionItem)
+    let canvasUnitPoints: CGFloat = width / CGFloat(partitionCount)
+    let layoutItems = discountItems.map {
+        layoutItem(for: $0, canvasUnitPointSize: canvasUnitPoints)
     }
     let groupedItems: [NSCollectionLayoutGroup] =  groupedRows(from: layoutItems, width: width)
    
