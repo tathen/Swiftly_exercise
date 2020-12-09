a6ba452c14239a6c90313c04ed11d2005ccd91aa
Sean Howell <tathen@yahoo.com>
2020-12-09 16:07:49 -0600

    Add SampleData.json to unit tests

--- ManagerSpecialsTests/ManagerSpecialsTests.swift
+++ ManagerSpecialsTests/ManagerSpecialsTests.swift
@@ -10,24 +10,65 @@ import XCTest
 
 class ManagerSpecialsTests: XCTestCase {
 
+    enum TestEnvironementError: Error {
+        case resourceNotFound
+        case dataNotAvailable
+    }
+    
+    var discountItems: [DiscountItem]!
+    var canvasPartitionCount: CanvasUnit!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let bundle = Bundle(for: Self.self)
+        guard let url = bundle.url(forResource: "SampleData", withExtension: "json") else {
+            throw TestEnvironementError.resourceNotFound
+        }
+        do {
+           let jsonData = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let endpointResponse = try decoder.decode(EndPointResponse.self, from: jsonData)
+            canvasPartitionCount = endpointResponse.canvasPartitions
+            discountItems = endpointResponse.managerSpecials
+        } catch {
+            throw TestEnvironementError.dataNotAvailable
+        }
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testSetup() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertNotNil(discountItems)
+        XCTAssertEqual(discountItems.count, 3)
+        XCTAssertNotNil(canvasPartitionCount)
+        XCTAssertEqual(canvasPartitionCount, 16)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGroupedRowsFromItems() {
+        let testWidth: CGFloat = 820
+        let noGroups = groupedRows(from: [], width: testWidth)
+        XCTAssert(noGroups.isEmpty)
+        let layoutItems = discountItems.map {
+            layoutItem(for: $0, canvasUnitPointSize: testWidth / CGFloat(canvasPartitionCount))
         }
+        
+        // instant line breaks will cause each item to be grouped individually
+        let noWidth = groupedRows(from: layoutItems, width: 0)
+        XCTAssertEqual(noWidth.count, 3)
+        
+        let groups = groupedRows(from: layoutItems, width: testWidth)
+        XCTAssertEqual(groups.count, 2)
     }
 
+    //collectionLayout(for discountItems: [DiscountItem], width: CGFloat, partitionCount: CanvasUnit) -> UICollectionViewLayout
+    func testCollectionLayoutForDiscountItems() {
+        let testWidth: CGFloat = 1000
+        let layout = collectionLayout(for: discountItems, width: testWidth, partitionCount: canvasPartitionCount)
+        XCTAssert(layout is UICollectionViewCompositionalLayout)
+      
+    }
 }
